3f59acdeef3edfb162d2429b7287eda13e83c62d
Charlie <mail@charlieharding.net>
2016-05-29 11:29:05 +1200

    First commit
    So far
    - Randomly generating landscape (placeholder hills)
    - Placeholder player model

---
+++ js/flyer.js
@@ -0,0 +1,357 @@
+// COLORS
+var Colors = {
+	purple:0x985AA6,
+	blue:0x6B839E,
+	pink:0xA6B5C5,
+};
+
+///////////////
+
+// GAME VARIABLES
+var game = {
+	distance: 0,
+	speed: .008,
+	rate: .000001,
+	width: 150,
+	height: 60,
+	gravity: .6
+}
+var player = {};
+var deltaTime = 0;
+var newTime = new Date().getTime();
+var oldTime = new Date().getTime();
+var obstaclePool = [];
+
+// SCENE VARIABLES
+var scene, camera, fieldOfView, aspectRatio, nearPlane, farPlane, HEIGHT, WIDTH, renderer, container, aspect, d;
+var hemisphereLight, shadowLight;
+
+//
+// CREATION FUNCTIONS
+//
+
+function createScene() {
+	HEIGHT = window.innerHeight;
+	WIDTH = window.innerWidth;
+	scene = new THREE.Scene();
+	
+	// Create the camera
+  aspectRatio = WIDTH / HEIGHT;
+  fieldOfView = 80;
+  nearPlane = 1;
+  farPlane = 1000;
+  camera = new THREE.PerspectiveCamera(
+    fieldOfView,
+    aspectRatio,
+    nearPlane,
+    farPlane
+    );
+  scene.fog = new THREE.Fog(Colors.pink, 0,500);
+
+  // Set camera x based on screen width
+  camera.position.x = -100
+  camera.position.y = +50
+  camera.lookAt(new THREE.Vector3(0,0,0))
+	// Create the renderer
+	renderer = new THREE.WebGLRenderer({ 
+		alpha: true,
+		antialias: true 
+	});
+	renderer.setSize(WIDTH, HEIGHT);
+	
+	// Enable shadow rendering
+	renderer.shadowMap.enabled = true;
+	container = document.getElementById('world');
+	container.appendChild(renderer.domElement);
+
+	// Reset the renderer on resize
+	window.addEventListener('resize', handleWindowResize, false);
+}
+
+function createLights() {
+	hemisphereLight = new THREE.HemisphereLight(0xcad5e3,0x070a0d, .8)
+	shadowLight = new THREE.DirectionalLight(0xf3e8ff, .8);
+
+	// Set the direction of the light  
+	shadowLight.position.set(200, 600, 400);
+	
+	// Allow shadow casting 
+	shadowLight.castShadow = true
+
+	// define the visible area of the projected shadow
+	shadowLight.shadow.camera.left = -100;
+	shadowLight.shadow.camera.right = 400;
+	shadowLight.shadow.camera.top = 400;
+	shadowLight.shadow.camera.bottom = -400;
+	shadowLight.shadow.camera.near = 1;
+	shadowLight.shadow.camera.far = 600;
+
+	// Define the resolution of the shadow; 
+	shadowLight.shadow.mapSize.width = 2048;
+	shadowLight.shadow.mapSize.height = 2048;
+	
+	scene.add(hemisphereLight);  
+	scene.add(shadowLight);
+}
+
+function createGround(){
+	ground = new Ground();
+	scene.add( ground.mesh );
+}
+
+function createPlayer(){
+	player.turnSpeed = .04;
+	player.moveSpeed = .05;
+	player.up = false;
+	player.down = false;
+	player.left = false;
+	player.right = false;
+	player.yaw = 0;
+	player.roll = 0;
+	player.pitch = 0;
+	player.pitchMax = 20;
+	player.pitchDelt = 0;
+  player.model = new Van();
+  player.model.mesh.position.y = +15;
+  player.model.mesh.position.x = -40;
+  scene.add(player.model.mesh);
+  //player.model.mesh.castShadow = true;
+}
+
+function createObstacle(){
+	var z = (Math.random() * (game.width - 20)) - (game.width/2 - 20)
+	obs = new Obstacle();
+	obs.mesh.position.z = z;
+	obs.mesh.position.x = +400;
+	obs.mesh.rotation.y = Math.random() * Math.PI*2
+	obstaclePool.push(obs)
+	scene.add( obs.mesh );
+}
+
+function createTerrain(){
+	ter = new Terrain();
+	ter.mesh.position.y = -1000;
+	scene.add(ter.mesh);
+	//hill = new Hill(50);
+	//scene.add(hill.mesh);
+}
+
+function removeEntity(object) {
+   // Remove an object from the scene
+   scene.remove( object.mesh );
+}
+
+//
+// MODELS
+//
+
+var Obstacle = function(){
+	var geom = new THREE.TetrahedronGeometry(8,1);
+  var mat = new THREE.MeshPhongMaterial({color:Colors.pink, shading:THREE.FlatShading});
+  this.mesh = new THREE.Mesh(geom,mat);
+  //this.mesh.castShadow = true;
+}
+
+var Ground = function(){
+	var geo = new THREE.CylinderGeometry( 1000, 1000, 600, 64 );
+  var mat = new THREE.MeshPhongMaterial({color:Colors.blue, shading:THREE.FlatShading});
+	this.mesh = new THREE.Mesh( geo, mat );
+	this.mesh.receiveShadow = true; 
+	this.mesh.position.y -= 1000;
+	this.mesh.rotation.x += Math.PI/2
+}
+
+var Terrain = function(){
+	this.mesh = new THREE.Object3D();
+	var density = 4 // Density of terrain (increase number of hills)
+	var rough = 150 // Set the scale of the hills
+	var num = Math.floor(3142 / rough*density)
+
+	for(var i=0; i<num; i++){
+		var right = new Hill(rough);
+		right.mesh.rotation.z += (Math.PI*2)/num * i;
+		this.mesh.add(right.mesh);
+	}
+
+}
+
+var Hill = function(val){
+	// Bad duplicate code here, should be replaced by better hill models anyway
+	var h1 = Math.random() * (val*1 - val/2) + val/2;
+	var w1 = Math.random() * (val*1 - val/2) + val/2;
+	var h2 = Math.random() * (val*1 - val/2) + val/2;
+	var w2 = Math.random() * (val*1 - val/2) + val/2;
+	var v1 = Math.random() * (val*.2) - val*.1
+	var v2 = Math.random() * (val*.1) + val*.1
+
+	this.mesh = new THREE.Object3D();
+	
+	var geom1 = new THREE.BoxGeometry( w1,h1,w1);
+	geom1.vertices[0].y -= v1;
+	geom1.vertices[4].y += v1;
+	geom1.vertices[4].x += v2;
+	geom1.vertices[4].z += v2;
+	var mat1 = new THREE.MeshPhongMaterial({color:Colors.blue});
+	var m1 = new THREE.Mesh( geom1, mat1 );
+	m1.position.z += (game.width/2) + w1 + Math.random()*(game.width*.1)
+	m1.position.y += (1000 + (h1/2)*.9)
+	m1.rotation.y += Math.random()*(Math.PI*2)
+
+	this.mesh.add(m1)
+
+	var geom2 = new THREE.BoxGeometry( w2,h2,w2);
+	geom2.vertices[0].y -= v1;
+	geom2.vertices[4].y += v1;
+	geom2.vertices[4].x += v2;
+	geom2.vertices[4].z += v2;
+	var mat2 = new THREE.MeshPhongMaterial({color:Colors.blue});
+	var m2 = new THREE.Mesh( geom2, mat2 );
+	m2.position.z -= ((game.width/2) + w2 + Math.random()*(game.width*.1))
+	m2.position.y += (1000 + (h2/2)*.9)
+	m2.rotation.y += Math.random()*(Math.PI*2)	
+	this.mesh.add(m2)
+	
+}
+
+var Van = function(){
+	this.mesh = new THREE.Object3D();
+  this.mesh.name = "wing";
+
+  // Van body
+	var gBody = new THREE.BoxGeometry( 20,4,8,1,1,1 );
+	var mBody = new THREE.MeshPhongMaterial({color:Colors.purple, shading:THREE.FlatShading});
+	var body = new THREE.Mesh( gBody, mBody );
+	body.position.x += 5;
+	body.castShadow = true;
+	this.mesh.add(body);
+
+	// Cab
+
+	//this.mesh.receiveShadow = true;
+	//this.mesh.castShadow = true;
+}
+
+//
+// UPDATE FUNCTIONS
+//
+
+function updateDistance(){
+  game.distance += game.speed
+  game.speed += game.rate
+}
+
+function updateObstacles(){
+	for (var i = 0; i < obstaclePool.length; i++) {
+  	obstaclePool[i].mesh.position.x -= game.speed;
+  	//console.log(obstaclePool[i]);
+	}
+}
+
+function updatePlayer(){
+	var yR = player.model.mesh.rotation.y;
+	var zP = player.model.mesh.position.z;
+	var yP = player.model.mesh.position.y;
+
+
+	// Reset camera position and aim
+  //var camX = 0.02*WIDTH - 127
+  //camera.position.x = camX;
+	//camera.lookAt(new THREE.Vector3(0,0,0))
+  //camera.updateProjectionMatrix();
+}
+
+function rotateWorld(){
+	ground.mesh.rotation.y += game.speed/10;
+	ter.mesh.rotation.z += game.speed/10;
+}
+
+//
+// EVENT HANDLERS
+//
+
+function handleWindowResize() {
+  HEIGHT = window.innerHeight;
+  WIDTH = window.innerWidth;
+  renderer.setSize(WIDTH, HEIGHT);
+  camera.aspect = WIDTH / HEIGHT;
+  camera.updateProjectionMatrix();
+}
+
+function handlekeydown(event){
+	var keyCode = event.keyCode;
+  switch(keyCode){
+  	case 87:  // w
+  		if(player.up){
+    		player.down = false;
+     	}
+    		player.up = true
+    break;
+    case 65:  // a
+    	if(player.right){
+    		player.right = false;
+     	}
+    		player.left = true
+    break;
+    case 83:  // s
+    	if(player.down){
+    		player.up = false;
+     	}
+    		player.down = true
+    break;
+    case 68:  // d
+   		if(player.left){
+    		player.left = false;
+     	}
+    		player.right = true
+    break;
+    case 32:  // Space
+    	createObstacle()
+    break;
+  }
+}
+
+function handlekeyup(event){
+	var keyCode = event.keyCode;
+  switch(keyCode){
+  	case 87:  //w
+  	break;
+    case 65:  //a
+    break;
+    case 83:  //s
+    break;
+    case 68:  //d
+    break;
+  }
+}
+
+//
+// GAME LOOP & INITIALISATION
+//
+
+function loop(){
+	// Update things
+	newTime = new Date().getTime();
+  deltaTime = newTime-oldTime;
+  oldTime = newTime;
+  updatePlayer();
+	updateDistance();
+	updateObstacles();
+	rotateWorld();
+	// Render the scene
+	renderer.render(scene, camera);
+	requestAnimationFrame(loop);
+}
+
+function init() {
+	createScene();
+	createLights();
+	createGround();
+	createTerrain();
+	createPlayer();
+	document.addEventListener('keydown', handlekeydown, false);
+	document.addEventListener('keyup', handlekeyup, false);
+	loop();
+
+}
+
+window.addEventListener('load', init, false);
