e9f01128b7fa0c19306452460830e06e7017f335
charlieharding <mail@charlieharding.net>
2016-05-31 16:53:55 +1200

    Improved phone controls
    - Movement is now scaled based on how far you tilt the phone

--- js/flyer.js
+++ js/flyer.js
@@ -20,7 +20,14 @@ var game = {
 	height: 90,
 	gravity: .6
 }
-var console;
+var tilt = {
+	// Orientaition of the phone
+	x: 1, // Beta
+	y: 1, // Gamma
+	z: 1  // Alpha
+}
+var helpText;	// For mobile debugging
+var isMobile = false;
 var cam = {
 	x: 0,
 	y: 60,
@@ -356,16 +363,16 @@ function updatePlayer(){
 
 	// Update player aim
 	if(player.up && player.aim.y < game.height){
-		player.aim.y += player.move*0.5
+		player.aim.y += (player.move*0.5) * tilt.y
 	}
 	if(player.down && player.aim.y > 10){
-		player.aim.y -= player.move*1.2
+		player.aim.y -= (player.move*1.2) * tilt.y
 	}
 	if(player.left && player.aim.x > -(game.width/2 +20)){
-		player.aim.x -= player.move
+		player.aim.x -= player.move*tilt.x
 	}
 	if(player.right && player.aim.x < game.width/2 +20){
-		player.aim.x += player.move
+		player.aim.x += player.move*tilt.x
 	}
 	if(player.showAim){
 		aim.mesh.position.z = player.aim.x
@@ -458,34 +465,45 @@ function handlekeyup(event){
 }
 
 function handleOrientation(event) {
-	var z = event.alpha;  // In degree in the range [-180,180]
-	var x = event.beta;  // In degree in the range [-180,180]
-  var y = event.gamma; // In degree in the range [-90,90]
+	var z = event.alpha;  
+	var x = event.beta;  
+  var y = event.gamma; 
 
-  console.innerHTML = "alpha: " + Math.round(z*100)/100 + "\n";
-  console.innerHTML += "beta : " + Math.round(x*100)/100 + "\n";
-  console.innerHTML += "gamma: " + Math.round(y*100)/100 + "\n";
+  //tilt.z = 
+	//tilt.x =
+  //tilt.y = 
+
+  //helpText.innerHTML = "alpha: " + Math.round(z*100)/100 + "\n";
+  //helpText.innerHTML += "beta : " + Math.round(x*100)/100 + "\n";
+  //helpText.innerHTML += "gamma: " + Math.round(y*100)/100 + "\n";
+  //helpText.innerHTML = "Mobile: " + isMobile + "\n";
     
   if(x > 10 && x < 50){
   	player.right = true;
   	player.left = false;
+  	tilt.x = .5 + (x * 0.02) //
   }else if(x < -10 && x > -50){
   	player.right = false;
   	player.left = true;
+  	tilt.x = .5 + (-x * 0.02)
   }else{
   	player.right = false;
   	player.left = false;
+  	tilt.x = 1
   }
 
-  if(y > -60 && y < -45){
+  if(y > -65 && y < -45){
   	player.up = true;
   	player.down = false;
-  }else if(y > -35 && y < -10){
+  	tilt.y = (y * -0.05) - 1.5
+  }else if(y > -30 && y < -10){
   	player.up = false;
   	player.down = true;
+  	tilt.y = (y * 0.05) + 2.0
   }else{
   	player.up = false;
   	player.down = false;
+  	tilt.y = 1
   }
 }
 
@@ -510,13 +528,14 @@ function loop(){
 }
 
 function init() {
+
 	createScene();
 	createLights();
 	createGround();
 	createTerrain();
 	createPlayer();
 	createCheckHolder();
-	console = document.querySelector('.console');
+	helpText = document.querySelector('.helpText');
 	document.addEventListener('keydown', handlekeydown, false);
 	document.addEventListener('keyup', handlekeyup, false);
 	window.addEventListener('deviceorientation', handleOrientation);
@@ -524,4 +543,7 @@ function init() {
 
 }
 
+// device detection
+if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
 window.addEventListener('load', init, false);
